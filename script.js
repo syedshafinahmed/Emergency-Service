51eed55cf86ababeb06150dc827f3f855e37163c
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-28 00:40:39 +0600

    added call button alert for all the buttons

--- script.js
+++ script.js
@@ -43,4 +43,88 @@ document.getElementById('call2').addEventListener("click", function(){
     }
 })
 
+document.getElementById('call3').addEventListener("click", function(){
+    let coin = document.getElementById("coins").innerText;
+    if(coin >= 20){
+        alert("📞 Calling Fire Service 999...");
+        coin -= 20;
+        document.getElementById('coins').innerText = coin;
+    }
+    else{
+        alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
+    }
+})
+
+document.getElementById('call4').addEventListener("click", function(){
+    let coin = document.getElementById("coins").innerText;
+    if(coin >= 20){
+        alert("📞 Calling Ambulance 1994-999999...");
+        coin -= 20;
+        document.getElementById('coins').innerText = coin;
+    }
+    else{
+        alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
+    }
+})
+
+document.getElementById('call5').addEventListener("click", function(){
+    let coin = document.getElementById("coins").innerText;
+    if(coin >= 20){
+        alert("📞 Calling Women & Child Helpline 109...");
+        coin -= 20;
+        document.getElementById('coins').innerText = coin;
+    }
+    else{
+        alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
+    }
+})
+
+document.getElementById('call6').addEventListener("click", function(){
+    let coin = document.getElementById("coins").innerText;
+    if(coin >= 20){
+        alert("📞 Calling Anti-Corruption Helpline 106...");
+        coin -= 20;
+        document.getElementById('coins').innerText = coin;
+    }
+    else{
+        alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
+    }
+})
+
+document.getElementById('call7').addEventListener("click", function(){
+    let coin = document.getElementById("coins").innerText;
+    if(coin >= 20){
+        alert("📞 Calling Electricity Helpline 16216...");
+        coin -= 20;
+        document.getElementById('coins').innerText = coin;
+    }
+    else{
+        alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
+    }
+})
+
+document.getElementById('call8').addEventListener("click", function(){
+    let coin = document.getElementById("coins").innerText;
+    if(coin >= 20){
+        alert("📞 Calling Brac Helpline 16445...");
+        coin -= 20;
+        document.getElementById('coins').innerText = coin;
+    }
+    else{
+        alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
+    }
+})
+
+document.getElementById('call9').addEventListener("click", function(){
+    let coin = document.getElementById("coins").innerText;
+    if(coin >= 20){
+        alert("📞 Calling Bangladesh Railway Helpline 163...");
+        coin -= 20;
+        document.getElementById('coins').innerText = coin;
+    }
+    else{
+        alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
+    }
+})
+
 
