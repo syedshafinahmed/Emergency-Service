4104aecdc31a8c2b08411f5b801ac2d29458a381
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-28 01:24:08 +0600

    added history functionality

--- script.js
+++ script.js
@@ -18,111 +18,234 @@ heartCounter("heart8");
 heartCounter("heart9");
 
 
-document.getElementById('call1').addEventListener("click", function(){
+
+
+
+
+document.getElementById('call1').addEventListener("click", function () {
     let coin = document.getElementById("coins").innerText;
-    if(coin >= 20){
+    if (coin >= 20) {
         alert("📞 Calling National Emergency Service 999...");
         coin -= 20;
         document.getElementById('coins').innerText = coin;
+        const now = new Date();
+        const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+        const section = document.createElement("section");
+        section.className = "p-5";
+        section.innerHTML = 
+            `<div class="bg-gray-300 rounded-xl p-5 flex justify-between items-center">
+                <div>
+                    <h1 class="font-bold text-sm">National Emergency</h1>
+                    <p class="text-[#5C5C5C] text-xs">999</p>
+                </div>
+                <div class="text-xs">${timeString}</div>
+            </div>`;
+        document.getElementById("call-history").appendChild(section);
     }
-    else{
+    else {
         alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
     }
 })
 
 
-document.getElementById('call2').addEventListener("click", function(){
+document.getElementById('call2').addEventListener("click", function () {
     let coin = document.getElementById("coins").innerText;
-    if(coin >= 20){
+    if (coin >= 20) {
         alert("📞 Calling Police Helpline Service 999...");
         coin -= 20;
         document.getElementById('coins').innerText = coin;
+        const now = new Date();
+        const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+        const section = document.createElement("section");
+        section.className = "p-5";
+        section.innerHTML = 
+            `<div class="bg-gray-300 rounded-xl p-5 flex justify-between items-center">
+                <div>
+                    <h1 class="font-bold text-sm">Police Helpline</h1>
+                    <p class="text-[#5C5C5C] text-xs">999</p>
+                </div>
+                <div class="text-xs">${timeString}</div>
+            </div>`;
+        document.getElementById("call-history").appendChild(section);
     }
-    else{
+    else {
         alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
     }
 })
 
-document.getElementById('call3').addEventListener("click", function(){
+document.getElementById('call3').addEventListener("click", function () {
     let coin = document.getElementById("coins").innerText;
-    if(coin >= 20){
+    if (coin >= 20) {
         alert("📞 Calling Fire Service 999...");
         coin -= 20;
         document.getElementById('coins').innerText = coin;
+        const now = new Date();
+        const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+        const section = document.createElement("section");
+        section.className = "p-5";
+        section.innerHTML = 
+            `<div class="bg-gray-300 rounded-xl p-5 flex justify-between items-center">
+                <div>
+                    <h1 class="font-bold text-sm">Fire Service</h1>
+                    <p class="text-[#5C5C5C] text-xs">999</p>
+                </div>
+                <div class="text-xs">${timeString}</div>
+            </div>`;
+        document.getElementById("call-history").appendChild(section);
     }
-    else{
+    else {
         alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
     }
 })
 
-document.getElementById('call4').addEventListener("click", function(){
+document.getElementById('call4').addEventListener("click", function () {
     let coin = document.getElementById("coins").innerText;
-    if(coin >= 20){
+    if (coin >= 20) {
         alert("📞 Calling Ambulance 1994-999999...");
         coin -= 20;
         document.getElementById('coins').innerText = coin;
+        const now = new Date();
+        const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+        const section = document.createElement("section");
+        section.className = "p-5";
+        section.innerHTML = 
+            `<div class="bg-gray-300 rounded-xl p-5 flex justify-between items-center">
+                <div>
+                    <h1 class="font-bold text-sm">Ambulance</h1>
+                    <p class="text-[#5C5C5C] text-xs">1994-999999</p>
+                </div>
+                <div class="text-xs">${timeString}</div>
+            </div>`;
+        document.getElementById("call-history").appendChild(section);
     }
-    else{
+    else {
         alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
     }
 })
 
-document.getElementById('call5').addEventListener("click", function(){
+document.getElementById('call5').addEventListener("click", function () {
     let coin = document.getElementById("coins").innerText;
-    if(coin >= 20){
+    if (coin >= 20) {
         alert("📞 Calling Women & Child Helpline 109...");
         coin -= 20;
         document.getElementById('coins').innerText = coin;
+        const now = new Date();
+        const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+        const section = document.createElement("section");
+        section.className = "p-5";
+        section.innerHTML = 
+            `<div class="bg-gray-300 rounded-xl p-5 flex justify-between items-center">
+                <div>
+                    <h1 class="font-bold text-sm">Women & Child Helpline</h1>
+                    <p class="text-[#5C5C5C] text-xs">109</p>
+                </div>
+                <div class="text-xs">${timeString}</div>
+            </div>`;
+        document.getElementById("call-history").appendChild(section);
     }
-    else{
+    else {
         alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
     }
 })
 
-document.getElementById('call6').addEventListener("click", function(){
+document.getElementById('call6').addEventListener("click", function () {
     let coin = document.getElementById("coins").innerText;
-    if(coin >= 20){
+    if (coin >= 20) {
         alert("📞 Calling Anti-Corruption Helpline 106...");
         coin -= 20;
         document.getElementById('coins').innerText = coin;
+        const now = new Date();
+        const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+        const section = document.createElement("section");
+        section.className = "p-5";
+        section.innerHTML = 
+            `<div class="bg-gray-300 rounded-xl p-5 flex justify-between items-center">
+                <div>
+                    <h1 class="font-bold text-sm">Anti-Corruption Helpline</h1>
+                    <p class="text-[#5C5C5C] text-xs">106</p>
+                </div>
+                <div class="text-xs">${timeString}</div>
+            </div>`;
+        document.getElementById("call-history").appendChild(section);
     }
-    else{
+    else {
         alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
     }
 })
 
-document.getElementById('call7').addEventListener("click", function(){
+document.getElementById('call7').addEventListener("click", function () {
     let coin = document.getElementById("coins").innerText;
-    if(coin >= 20){
+    if (coin >= 20) {
         alert("📞 Calling Electricity Helpline 16216...");
         coin -= 20;
         document.getElementById('coins').innerText = coin;
+        const now = new Date();
+        const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+        const section = document.createElement("section");
+        section.className = "p-5";
+        section.innerHTML = 
+            `<div class="bg-gray-300 rounded-xl p-5 flex justify-between items-center">
+                <div>
+                    <h1 class="font-bold text-sm">Electricity Helpline</h1>
+                    <p class="text-[#5C5C5C] text-xs">16216</p>
+                </div>
+                <div class="text-xs">${timeString}</div>
+            </div>`;
+        document.getElementById("call-history").appendChild(section);
     }
-    else{
+    else {
         alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
     }
 })
 
-document.getElementById('call8').addEventListener("click", function(){
+document.getElementById('call8').addEventListener("click", function () {
     let coin = document.getElementById("coins").innerText;
-    if(coin >= 20){
+    if (coin >= 20) {
         alert("📞 Calling Brac Helpline 16445...");
         coin -= 20;
         document.getElementById('coins').innerText = coin;
+        const now = new Date();
+        const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+        const section = document.createElement("section");
+        section.className = "p-5";
+        section.innerHTML = 
+            `<div class="bg-gray-300 rounded-xl p-5 flex justify-between items-center">
+                <div>
+                    <h1 class="font-bold text-sm">Brac Helpline</h1>
+                    <p class="text-[#5C5C5C] text-xs">16445</p>
+                </div>
+                <div class="text-xs">${timeString}</div>
+            </div>`;
+        document.getElementById("call-history").appendChild(section);
     }
-    else{
+    else {
         alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
     }
 })
 
-document.getElementById('call9').addEventListener("click", function(){
+
+
+document.getElementById('call9').addEventListener("click", function () {
     let coin = document.getElementById("coins").innerText;
-    if(coin >= 20){
+    if (coin >= 20) {
         alert("📞 Calling Bangladesh Railway Helpline 163...");
         coin -= 20;
         document.getElementById('coins').innerText = coin;
+        const now = new Date();
+        const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+        const section = document.createElement("section");
+        section.className = "p-5";
+        section.innerHTML = 
+            `<div class="bg-gray-300 rounded-xl p-5 flex justify-between items-center">
+                <div>
+                    <h1 class="font-bold text-sm">Bangladesh Railway Helpline</h1>
+                    <p class="text-[#5C5C5C] text-xs">163</p>
+                </div>
+                <div class="text-xs">${timeString}</div>
+            </div>`;
+        document.getElementById("call-history").appendChild(section);
     }
-    else{
+    else {
         alert("❌ Insufficient Coin. You need atleast 20 coins to proceed!")
     }
 })
